b760385dcea6fa37737579047e25ba4bee202308
kansai-pg <contact@japan-is.fun>
2021-10-30 16:02:15 +0900

    1030

--- linebot-web/view.php
+++ linebot-web/view.php
@@ -2,7 +2,6 @@
 session_start();
 ini_set('display_errors', 1);
 include('login_class.php');
-include('db_class.php');
 
 if (empty($_SESSION['state']) or empty($_SESSION['state'])){
     header("Location: index.html");
@@ -20,9 +19,7 @@ if (empty($_SESSION['state']) or empty($_SESSION['state'])){
 
 $user_id = $_SESSION['user_id'];
 
-#require('./dbconnect.php');
-
-$dbh = new db();
+require('./dbconnect.php');
 
 #https://gray-code.com/php/getting-data-by-using-pdo/
 $stmt = $dbh->prepare("SELECT id, room_name, time, datetime, frequency FROM mainid WHERE user_id = :user_id");

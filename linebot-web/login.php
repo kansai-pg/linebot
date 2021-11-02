1bb6d7a8d04d218f25a4a23d7b5c84e1c8f03eb9
kansai-pg <contact@japan-is.fun>
2021-11-02 12:07:23 +0900

    1102

--- linebot-web/login.php
+++ linebot-web/login.php
@@ -4,7 +4,7 @@ session_start();
 $bytes = openssl_random_pseudo_bytes(5);
 $_SESSION['state'] = bin2hex($bytes);
 session_write_close();
-$sendurl = "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1656389284&redirect_uri=https://linebot-web.japan-is.fun/view.php&state=" . $_SESSION['state'] .  "&scope=profile";
+$sendurl = "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1656389284&redirect_uri=https://linebot.japan-is.fun/view.php&state=" . $_SESSION['state'] .  "&scope=profile";
 #https://take-lab.com/2017/12/04/post-317/
 header('Location: ' . $sendurl, true, 301);
 exit;

686cd090b546ccb5c089bdd59fe5bae9a9cf985c
kansai-pg <contact@japan-is.fun>
2021-11-02 12:17:40 +0900

    1102

--- linebot-web/edit.php
+++ linebot-web/edit.php
@@ -101,9 +101,9 @@ $room_datas = $stmt->fetchAll();
                 </div>
                 <div class="d-flex align-items-center">
                     <button type="submit" class="btn btn-primary mx-auto" style="width: 50%; margin: 20px;">送信</button>
+            </form>
                     <button onclick="location.href='view.php'" class="btn btn-primary">一覧表示へ戻る</button>
                 </div>
-            </form>
 
         </div>
     </div>

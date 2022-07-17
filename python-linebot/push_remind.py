d5dcaaec7ef58eecb26fbe7cd37173fc36d93ac4
yep i'm student <contact@japan-is.fun>
2022-07-18 04:35:49 +0900

    Added push notification to long-term non-responders

--- python-linebot/push_remind.py
+++ python-linebot/push_remind.py
@@ -195,4 +195,131 @@ def scheduled_job():
         #DB接続エラーの時にエラー内容とともに自分のLineへ通知が行く
         push_error.send_error(e, my_user_id)
 
+def long_term_non_responders():
+    day = datetime.date.today()
+    hour = int(datetime.datetime.now().strftime('%H'))
+    try:
+        with get_connection() as conn:
+            with conn.cursor() as cur:
+                print(day, hour)
+                cur.execute("select user_id, room_name, TO_CHAR(lastpush,'yyyy/mm/dd'), ID from mainid WHERE LASTPUSH <= ADD_MONTHS(:0, -3) and status != 6", (day,))
+                for col in cur:
+                    #jsonで送信内容を書く
+                    req_data = {
+                        "to": col[0],
+                        "messages": [
+                            {
+                            "type": "flex",
+                            "altText": col[1] + "掃除に関する返答が長期間ありません。",
+                            "contents": {
+                            "type": "bubble",
+                            "size": "mega",
+                            "direction": "ltr",
+                            "header": {
+                                "type": "box",
+                                "layout": "vertical",
+                                "contents": [
+                                {
+                                    "type": "text",
+                                    "text": col[1] + "掃除に関する返答が長期間ありません。",
+                                    "align": "center",
+                                }
+                                ]
+                            },
+                            "body": {
+                                "type": "box",
+                                "layout": "vertical",
+                                "contents": [
+                                            {
+                                            "type": "text",
+                                            "text": col[1] + "掃除に関する返答が三ヶ月以上ありません。\n",
+                                            "align": "center",
+                                            "wrap": True,
+                                            },
+                                            {
+                                            "type": "text",
+                                            "text": "前回の掃除日は" + str(col[2]) + "です\n",
+                                            "align": "center",
+                                            "wrap": True,
+                                            },
+                                            {
+                                            "type": "text",
+                                            "text": "※i一度も返答が無い場合は\n",
+                                            "align": "center",
+                                            "wrap": True,
+                                            },
+                                            {
+                                            "type": "text",
+                                            "text": "登録日が表示されます\n",
+                                            "align": "center",
+                                            "wrap": True,
+                                            },
+                                            {
+                                            "type": "text",
+                                            "text": "このまま返答がない場合は\n",
+                                            "align": "center",
+                                            "wrap": True,
+                                            },
+                                            {
+                                            "type": "text",
+                                            "text": "この名称への通知情報が自動削除されます\n",
+                                            "align": "center",
+                                            "wrap": True,
+                                            }
+                                ]
+                            },
+                            "footer": {
+                                "type": "box",
+                                "layout": "horizontal",
+                                "contents": [
+                                {
+                                    "type": "button",
+                                    "action": {
+                                    "type": "message",
+                                    "label": "削除しない",
+                                    "text": str(col[3]) + ",NODEL"
+                                    }
+                                },
+                                {
+                                    "type": "button",
+                                    "action": {
+                                    "type": "message",
+                                    "label": "削除する",
+                                    "text": str(col[3]) + ",DEL"
+                                    }
+                                }
+                                ]
+                            }
+                            }
+                            }
+                        ]
+                            }
+
+                    #ステータスコード更新
+                    with get_connection() as conn:
+                        with conn.cursor() as cur:
+                            cur.execute("UPDATE mainid SET status = 6 WHERE id= :1", (col[3],))
+                            conn.commit()
+
+                    push = urllib.request.Request(url, data=json.dumps(req_data).encode(), method='POST', headers=req_header)
+
+                    try:
+                        with urllib.request.urlopen(push) as response:
+                            body = json.loads(response.read())
+                            headers = response.getheaders()
+                            status = response.getcode()
+
+                            print(headers)
+                            print(status)
+
+                    except urllib.error.URLError as e:
+                        print(e.reason)
+                        push_error.send_error(e.reason, my_user_id)
+
+    except cx_Oracle.DatabaseError as e:
+        #DB接続エラーの時にエラー内容とともに自分のLineへ通知が行く
+        push_error.send_error(e, my_user_id)
+
+
+long_term_non_responders()
 scheduled_job()
